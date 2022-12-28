39e744eaaced1658f97d2ffa36bd67323dc9fb41
literat <tomas@litera.me>
2022-12-28 21:27:16 +0100

    Style: Homepage with family intersection

---
+++ components/FamilyMemberCard/FamilyMemberCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import styles from './FamilyMemberCard.module.scss';
+
+interface FamilyMemberCardProps {
+  link: string;
+  icon: string;
+  name: string;
+  description: string;
+}
+
+export default function FamilyMemberCard({ link, icon, name, description }: FamilyMemberCardProps) {
+  return (
+    <a
+      href={link}
+      className={styles.card}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2>{icon} <span>{name}</span></h2>
+      <p>{description}</p>
+    </a>
+  )
+}
