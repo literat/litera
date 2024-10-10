a5c729e287c70954dae3ce6f64dd193993528350
literat <tomas@litera.me>
2024-10-10 20:23:47 +0200

    fix: migrate from deprecated next/font package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
+import { Inter } from 'next/font/google'
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import FamilyMemberCard from '../components/FamilyMemberCard/FamilyMemberCard'
 import styles from '../styles/Home.module.scss'
 
