e055457c22359f10bd6faad5f1abce16be007f45
literat <tomas@litera.me>
2024-11-12 13:49:20 +0100

    chore(deps): switch from sass to sass-embedded
      * @see: https://nextjs.org/docs/pages/building-your-application/styling/sass#implementation
      * @see: https://sass-lang.com/documentation/breaking-changes/legacy-js-api/

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    implementation: 'sass-embedded',
+  },
 }
 
 module.exports = nextConfig
