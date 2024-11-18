e66b00701bc0a621cc29801293bf77d8ad216c80
literat <tomas@litera.me>
2024-11-18 22:23:19 +0100

    refactor: introduce shared root layout

--- src/app/(home)/home/page.tsx
+++ src/app/(home)/home/page.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default function Home() {
   return (
--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -1,22 +1,6 @@
-import { Metadata } from 'next';
 import { ReactNode } from 'react';
-import '@local/styles/globals.scss';
-import { inter } from '@local/ui/fonts';
-import React from 'react';
-
-export const metadata: Metadata = {
-  title: {
-    template: '%s | Litera.me',
-    default: 'Litera.me',
-  },
-  description: 'Litera Family',
-  metadataBase: new URL('https://litera.me'),
-};
+import '@local/ui/homepage.globals.scss';
 
 export default function HomepageLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
-  );
+  return children;
 }
