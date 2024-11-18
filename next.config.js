760ee0a254cee65ec0d33c7b4abae69dd1d1dbfa
literat <tomas@litera.me>
2024-11-18 18:56:32 +0100

    chore: silence errors from sass legacy api

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     implementation: 'sass-embedded',
+    // Setting api to use modern compiler do not work
+    // api: 'modern-compiler',
+    silenceDeprecations: ['legacy-js-api'],
   },
 }
 
